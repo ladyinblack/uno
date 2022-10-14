e76873c1ea93e9c60c391aab98dbc98f66738012
Jerome Laban <jerome.laban@nventive.com>
2022-10-14 09:07:14 -0400

    feat(assets): Adds support for content assets in NuGet and transive project refs

---
+++ src/SourceGenerators/Uno.UI.Tasks/Assets/ExpandPackageAssets.cs
@@ -0,0 +1,57 @@
+﻿#nullable enable
+
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Uno.UI.Tasks.Assets
+{
+	/// <summary>
+	/// Expands content assets from folders containing an upri.marker file
+	/// </summary>
+	public class ExpandPackageAssets_v0 : Task
+	{
+		[Required]
+		public ITaskItem[]? MarkerFiles { get; set; }
+
+		[Output]
+		public ITaskItem[]? Assets { get; set; }
+
+		public override bool Execute()
+		{
+			Log.LogMessage($"Expanding package assets");
+
+			if (MarkerFiles != null)
+			{
+				List<ITaskItem> assets = new();
+
+				foreach (var markerFile in MarkerFiles)
+				{
+					var markerFileFullPath = markerFile.GetMetadata("FullPath");
+					var markerFileDIrectory = Path.GetDirectoryName(markerFileFullPath);
+					var basePath = Path.Combine(markerFileDIrectory, Path.GetFileNameWithoutExtension(markerFileFullPath));
+
+					if (Directory.Exists(basePath))
+					{
+						foreach (var asset in Directory.EnumerateFiles(basePath, "*.*", SearchOption.AllDirectories))
+						{
+							var newItem = new TaskItem(
+								asset,
+								new Dictionary<string, string>
+								{
+									["TargetPath"] = asset.Replace(markerFileDIrectory, "")
+								});
+
+							assets.Add(newItem);
+						}
+					}
+				}
+
+				Assets = assets.ToArray();
+			}
+
+			return true;
+		}
+	}
+}
